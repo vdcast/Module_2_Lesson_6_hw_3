8512d3010918c6b48332fb9fbdf361ca0ae57046
VD <vdfree448@protonmail.com>
2022-07-26 20:26:54 +0300

    init

---
+++ app/src/main/java/com/example/module_2_lesson_6_hw_3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.module_2_lesson_6_hw_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
